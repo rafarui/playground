942aff9c304188c4ae1c96a58ae819a40326b4a3
Rafael Rui <rafarui@users.noreply.github.com>
2018-10-28 10:05:19 -0300

    Create primereduction.py

---
+++ kattis/primereduction.py
@@ -0,0 +1,48 @@
+import sys,math
+
+def sieve(n):
+    """
+    Sieve of Eratosthenes, return primes less than n
+    """
+    mark = [True for i in range(n+1)]
+    p=2
+    while(p*p <= n ):
+        if (mark[p] == True):
+            for i in range(2*p,n+1,p):
+                mark[i] = False
+        p +=1
+
+    primes = []
+    for i in range(2,len(mark)):
+        if mark[i]:
+            primes.append(i)
+
+    return primes
+
+def prime_red(n,primes):
+    count_iter = 0
+    while (True):
+        count_iter += 1
+        _sum = 0
+        sqrt_n = int(n ** 0.5)
+        for i in primes:
+            if (i > sqrt_n or n == 1):
+                break
+            while not n % i: 
+                n = n/i
+                _sum += i
+        if n != 1 and _sum != 0:
+            _sum += n
+        if _sum == 0:
+            break
+        n = _sum
+
+    print n,count_iter
+
+
+primes = sieve(int(10 ** (9 *0.5)) + 1)
+for line in sys.stdin:
+    line = int(line)
+    if line==4:
+        break
+    prime_red(line,primes)
